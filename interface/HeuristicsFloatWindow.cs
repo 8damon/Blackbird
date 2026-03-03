e29b1436fa45572149f64954780d9f6b827726ef
dutchpsycho <178704185+dutchpsycho@users.noreply.github.com>
2026-03-03 23:29:00 +1000

    ui/heuristics: add detections pane counters and floating host

---
+++ interface/HeuristicsFloatWindow.cs
@@ -0,0 +1,19 @@
+using System.Windows;
+
+namespace SleepwalkerInterface
+{
+    public sealed class HeuristicsFloatWindow : Window
+    {
+        public HeuristicsFloatWindow(HeuristicsPane pane)
+        {
+            Title = "Heuristics (Floating)";
+            Width = 980;
+            Height = 520;
+            Background = pane.Background;
+            Content = pane;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            WindowThemeHelper.ApplyDarkTitleBar(this);
+        }
+    }
+}
+

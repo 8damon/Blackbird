fe124b369191d3f2ade6a30ef126ae7f92e4407d
8damon <178704185+8damon@users.noreply.github.com>
2026-03-05 23:59:19 +1000

    kernel/control: extend telemetry schema and control export plumbing

--- kernel/core/control.h
+++ kernel/core/control.h
@@ -12,15 +12,7 @@ VOID SLEEPWALKERControlUninitialize(VOID);
 
 VOID SLEEPWALKERControlBeginShutdown(VOID);
 
-VOID SLEEPWALKERControlPublishHandleEvent(_In_ UINT64 CallerPid, _In_ UINT64 TargetPid, _In_ UINT32 DesiredAccess,
-                                          _In_ UINT32 ClassId, _In_ UINT64 OriginAddress, _In_ UINT32 OriginProtect,
-                                          _In_ UINT32 Flags, _In_opt_z_ PCWSTR OriginPath, _In_ UINT32 FrameCount,
-                                          _In_reads_opt_(FrameCount) PVOID const *Frames, _In_ INT32 StatusOpenProcess,
-                                          _In_ INT32 StatusBasicInfo, _In_ INT32 StatusSectionName,
-                                          _In_ UINT64 DeepAllocationBase, _In_ UINT64 DeepRegionSize,
-                                          _In_ UINT32 DeepRegionProtect, _In_ UINT32 DeepRegionState,
-                                          _In_ UINT32 DeepRegionType, _In_ UINT32 DeepSampleSize,
-                                          _In_reads_bytes_opt_(DeepSampleSize) const UCHAR *DeepSample);
+VOID SLEEPWALKERControlPublishHandleEvent(_In_ const SLEEPWALKER_HANDLE_EVENT *HandleEvent);
 
 VOID SLEEPWALKERControlPublishThreadEvent(_In_ UINT64 ProcessId, _In_ UINT64 ThreadId, _In_ UINT64 CreatorPid,
                                           _In_ UINT64 StartAddress, _In_ UINT64 ImageBase, _In_ UINT64 ImageSize,
@@ -29,5 +21,11 @@ VOID SLEEPWALKERControlPublishThreadEvent(_In_ UINT64 ProcessId, _In_ UINT64 Thr
 
 BOOLEAN
 SLEEPWALKERControlSelfCheck(VOID);
+
+BOOLEAN
+SLEEPWALKERControlHasClientsFast(VOID);
+
+BOOLEAN
+SLEEPWALKERControlHasPidInterest(_In_ UINT32 PrimaryProcessId, _In_ UINT32 SecondaryProcessId, _In_ UINT32 StreamMask);
 
 #endif
---
+++ kernel/core/pool_compat.h
@@ -0,0 +1,40 @@
+#ifndef SLEEPWALKER_POOL_COMPAT_H
+#define SLEEPWALKER_POOL_COMPAT_H
+
+typedef PVOID(NTAPI *SLEEPWALKER_EX_ALLOCATE_POOL2_FN)(_In_ POOL_FLAGS Flags, _In_ SIZE_T NumberOfBytes, _In_ ULONG Tag);
+
+static __forceinline PVOID SLEEPWALKERAllocatePoolCompat(_In_ POOL_FLAGS Flags, _In_ SIZE_T NumberOfBytes, _In_ ULONG Tag)
+{
+    static volatile LONG resolved = 0;
+    static SLEEPWALKER_EX_ALLOCATE_POOL2_FN allocatePool2 = NULL;
+
+    if (InterlockedCompareExchange(&resolved, 0, 0) == 0)
+    {
+        UNICODE_STRING name;
+
+        RtlInitUnicodeString(&name, L"ExAllocatePool2");
+        allocatePool2 = (SLEEPWALKER_EX_ALLOCATE_POOL2_FN)MmGetSystemRoutineAddress(&name);
+        InterlockedExchange(&resolved, 1);
+    }
+
+    if (allocatePool2 != NULL)
+    {
+        return allocatePool2(Flags, NumberOfBytes, Tag);
+    }
+
+    {
+        POOL_TYPE poolType;
+        PVOID allocation;
+
+        poolType = ((Flags & POOL_FLAG_PAGED) != 0) ? PagedPool : NonPagedPoolNx;
+        allocation = ExAllocatePoolWithTag(poolType, NumberOfBytes, Tag);
+        if (allocation != NULL && (Flags & POOL_FLAG_UNINITIALIZED) == 0)
+        {
+            RtlZeroMemory(allocation, NumberOfBytes);
+        }
+        return allocation;
+    }
+}
+
+#endif
+

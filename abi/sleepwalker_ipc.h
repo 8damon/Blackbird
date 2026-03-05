4a4f759c1e55b1070fac87664f7a96b1cff00f7d
dutchpsycho <178704185+dutchpsycho@users.noreply.github.com>
2026-03-05 23:59:19 +1000

    kernel/control: extend telemetry schema and control export plumbing

--- abi/sleepwalker_ipc.h
+++ abi/sleepwalker_ipc.h
@@ -25,7 +25,8 @@ typedef enum _SLEEPWALKER_IPC_COMMAND
     SleepwalkerIpcCommandGetStats = 6,
     SleepwalkerIpcCommandQueryProcessImage = 7,
     SleepwalkerIpcCommandSetShutdownMode = 8,
-    SleepwalkerIpcCommandGetEtwEvent = 9
+    SleepwalkerIpcCommandGetEtwEvent = 9,
+    SleepwalkerIpcCommandOpenSharedRing = 10
 } SLEEPWALKER_IPC_COMMAND;
 
 typedef struct _SLEEPWALKER_IPC_HANDSHAKE_REQUEST
@@ -46,6 +47,36 @@ typedef struct _SLEEPWALKER_IPC_GET_EVENT_REQUEST
     UINT32 TimeoutMs;
 } SLEEPWALKER_IPC_GET_EVENT_REQUEST, *PSLEEPWALKER_IPC_GET_EVENT_REQUEST;
 
+typedef struct _SLEEPWALKER_IPC_OPEN_SHARED_RING_REQUEST
+{
+    UINT32 DesiredIoctlCapacity;
+    UINT32 DesiredEtwCapacity;
+} SLEEPWALKER_IPC_OPEN_SHARED_RING_REQUEST, *PSLEEPWALKER_IPC_OPEN_SHARED_RING_REQUEST;
+
+typedef struct _SLEEPWALKER_IPC_OPEN_SHARED_RING_RESPONSE
+{
+    UINT64 IoctlMappingHandle;
+    UINT64 IoctlDataReadyEventHandle;
+    UINT32 IoctlCapacity;
+    UINT32 IoctlRecordSize;
+    UINT64 EtwMappingHandle;
+    UINT64 EtwDataReadyEventHandle;
+    UINT32 EtwCapacity;
+    UINT32 EtwRecordSize;
+} SLEEPWALKER_IPC_OPEN_SHARED_RING_RESPONSE, *PSLEEPWALKER_IPC_OPEN_SHARED_RING_RESPONSE;
+
+typedef struct _SLEEPWALKER_IPC_SHARED_RING_HEADER
+{
+    volatile LONG WriteIndex;
+    volatile LONG ReadIndex;
+    volatile LONG DroppedCount;
+    LONG Reserved0;
+    UINT32 Capacity;
+    UINT32 RecordSize;
+    UINT32 Reserved1;
+    UINT32 Reserved2;
+} SLEEPWALKER_IPC_SHARED_RING_HEADER, *PSLEEPWALKER_IPC_SHARED_RING_HEADER;
+
 typedef enum _SLEEPWALKER_IPC_ETW_SOURCE
 {
     SleepwalkerIpcEtwSourceUnknown = 0,
@@ -55,6 +86,7 @@ typedef enum _SLEEPWALKER_IPC_ETW_SOURCE
 
 #define SLEEPWALKER_IPC_MAX_ETW_EVENT_NAME 96
 #define SLEEPWALKER_IPC_MAX_ETW_DETECTION_NAME 128
+#define SLEEPWALKER_IPC_MAX_ETW_REASON 256
 
 typedef struct _SLEEPWALKER_IPC_ETW_EVENT
 {
@@ -71,6 +103,11 @@ typedef struct _SLEEPWALKER_IPC_ETW_EVENT
     UINT64 SecondaryPid;
     WCHAR EventName[SLEEPWALKER_IPC_MAX_ETW_EVENT_NAME];
     CHAR DetectionName[SLEEPWALKER_IPC_MAX_ETW_DETECTION_NAME];
+    UINT32 CorrelationFlags;
+    UINT32 CorrelationAccessMask;
+    UINT32 CorrelationAgeMs;
+    UINT32 Reserved2;
+    WCHAR Reason[SLEEPWALKER_IPC_MAX_ETW_REASON];
 } SLEEPWALKER_IPC_ETW_EVENT, *PSLEEPWALKER_IPC_ETW_EVENT;
 
 typedef union _SLEEPWALKER_IPC_PAYLOAD
@@ -81,6 +118,8 @@ typedef union _SLEEPWALKER_IPC_PAYLOAD
     SLEEPWALKER_UNSUBSCRIBE_REQUEST UnsubscribeRequest;
     SLEEPWALKER_SET_PIDS_REQUEST SetPidsRequest;
     SLEEPWALKER_IPC_GET_EVENT_REQUEST GetEventRequest;
+    SLEEPWALKER_IPC_OPEN_SHARED_RING_REQUEST OpenSharedRingRequest;
+    SLEEPWALKER_IPC_OPEN_SHARED_RING_RESPONSE OpenSharedRingResponse;
     SLEEPWALKER_EVENT_RECORD EventRecord;
     SLEEPWALKER_IPC_ETW_EVENT EtwEvent;
     SLEEPWALKER_STATS_RESPONSE StatsResponse;
@@ -102,5 +141,6 @@ typedef struct _SLEEPWALKER_IPC_PACKET
 #define SLEEPWALKER_IPC_CAP_DRIVER_PROXY 0x00000001u
 #define SLEEPWALKER_IPC_CAP_ETW_TI_SESSION 0x00000002u
 #define SLEEPWALKER_IPC_CAP_ETW_TI_UPLINK 0x00000004u
+#define SLEEPWALKER_IPC_CAP_SHARED_RING 0x00000008u
 
 #endif

77b82bb25d0db7a83984decd3a1e37233595effb
8damon <178704185+8damon@users.noreply.github.com>
2026-05-06 09:06:44 +1000

    kernel/monitors: update anti-tamper and APC monitors

--- Kernel/monitors/anti_tamper.h
+++ Kernel/monitors/anti_tamper.h
@@ -1,13 +1,15 @@
-#ifndef BLACKBIRD_ANTI_TAMPER_H
-#define BLACKBIRD_ANTI_TAMPER_H
+#ifndef BK_ANTI_TAMPER_H
+#define BK_ANTI_TAMPER_H
 
 #include <ntdef.h>
 
 NTSTATUS
-BLACKBIRDAntiTamperInitialize(_In_ PDRIVER_OBJECT DriverObject);
+BkatInitialize(_In_ PDRIVER_OBJECT DriverObject);
 
-VOID BLACKBIRDAntiTamperUninitialize(VOID);
+VOID BkatUninitialize(VOID);
 
-ULONG BLACKBIRDAntiTamperGetLastMask(VOID);
+ULONG BkatGetLastMask(VOID);
+
+BOOLEAN BkatSelfCheck(VOID);
 
 #endif
--- Kernel/monitors/apc_monitor.h
+++ Kernel/monitors/apc_monitor.h
@@ -1,17 +1,17 @@
-#ifndef BLACKBIRD_APC_MONITOR_H
-#define BLACKBIRD_APC_MONITOR_H
+#ifndef BK_APC_MONITOR_H
+#define BK_APC_MONITOR_H
 
 #include <ntdef.h>
 
 NTSTATUS
-BLACKBIRDApcMonitorInitialize(VOID);
+BkapcInitialize(VOID);
 
-VOID BLACKBIRDApcMonitorUninitialize(VOID);
+VOID BkapcUninitialize(VOID);
 
-VOID BLACKBIRDApcMonitorRecordThreadHandleIntent(_In_ HANDLE CallerPid, _In_ HANDLE TargetPid,
-                                                 _In_ ACCESS_MASK DesiredAccess, _In_ BOOLEAN IsDuplicateOperation);
+VOID BkapcRecordThreadHandleIntent(_In_ HANDLE CallerPid, _In_ HANDLE TargetPid, _In_ ACCESS_MASK DesiredAccess,
+                                   _In_ BOOLEAN IsDuplicateOperation);
 
 BOOLEAN
-BLACKBIRDApcMonitorSelfCheck(VOID);
+BkapcSelfCheck(VOID);
 
 #endif

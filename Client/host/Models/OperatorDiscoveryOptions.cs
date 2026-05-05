395895f3bb24c914c40dbae0b7a549201f25c84a
8damon <178704185+8damon@users.noreply.github.com>
2026-05-06 09:11:30 +1000

    ui/host: remove legacy operator shell

--- Client/host/Models/OperatorDiscoveryOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace BlackbirdOperator.Models;
-
-public sealed class OperatorDiscoveryOptions
-{
-    public bool EnablePassiveDiscovery { get; init; } = true;
-    public bool EnableActiveSubnetProbe { get; init; }
-    public string? PreferredInterfaceId { get; init; }
-    public int MaxProbeHosts { get; init; } = 256;
-}

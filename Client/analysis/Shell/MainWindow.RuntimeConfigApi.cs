6d1971cbe5b13e383a39c1f8ada899023ccf6579
8damon <178704185+8damon@users.noreply.github.com>
2026-05-06 09:11:19 +1000

    ui/services: add runtime configuration service

--- Client/analysis/Shell/MainWindow.RuntimeConfigApi.cs
+++ Client/analysis/Shell/MainWindow.RuntimeConfigApi.cs
@@ -4,17 +4,17 @@ namespace BlackbirdInterface
 {
     public partial class MainWindow
     {
-        internal bool ApplyStartupRuntimeSelections(bool enableKernelHooks, bool enableAntiVirtualizationMasking, bool enableControllerConcealment,
-                                                    bool enableInterfaceProtectedAccess, bool enableControllerProtectedAccess, out string error)
+        internal bool ApplyStartupRuntimeSelections(bool enableKernelHooks, bool enableAntiVirtualizationMasking,
+                                                    bool enableQpcTimingCompensation, bool enableControllerConcealment,
+                                                    bool enableInterfaceProtectedAccess,
+                                                    bool enableControllerProtectedAccess, out string error)
         {
-            var profile = new LaunchProfile
-            {
-                EnableKernelHooks = enableKernelHooks,
-                EnableAntiVirtualizationMasking = enableAntiVirtualizationMasking,
-                EnableControllerConcealment = enableControllerConcealment,
-                EnableInterfaceProtectedAccess = enableInterfaceProtectedAccess,
-                EnableControllerProtectedAccess = enableControllerProtectedAccess
-            };
+            var profile = new LaunchProfile { EnableKernelHooks = enableKernelHooks,
+                                              EnableAntiVirtualizationMasking = enableAntiVirtualizationMasking,
+                                              EnableQpcTimingCompensation = enableQpcTimingCompensation,
+                                              EnableControllerConcealment = enableControllerConcealment,
+                                              EnableInterfaceProtectedAccess = enableInterfaceProtectedAccess,
+                                              EnableControllerProtectedAccess = enableControllerProtectedAccess };
 
             return ApplySelectedRuntimeConfig(profile, out error);
         }

8afcb1baa31a6599cafe9c74526436794412ef80
8damon <8damon@users.noreply.github.com>
2026-08-16 00:37:25 +0200

    Rebrand TITAN as RYFTENIUS

--- Client/analysis/Settings/AnalystSettingsStore.cs
+++ Client/analysis/Settings/AnalystSettingsStore.cs
@@ -6,7 +6,7 @@ namespace BlackbirdInterface
 {
     internal static class AnalystSettingsStore
     {
-        private const string RootKeyPath = @"Software\TITAN Softwork Solutions\BK\Interface";
+        private const string RootKeyPath = @"Software\RYFTENIUS\BK\Interface";
         private const string ThemeValueName = "ThemeMode";
         private const string IgnoreVmSafetyWarningValueName = "IgnoreVmSafetyWarning";
         private const string ShortcutKeyPath = RootKeyPath + @"\Shortcuts";
